73ba09909dc9a8b7b7b0bd9a16cbee9247c2a299
matthewvarga <matthew.l.varga@gmail.com>
2020-05-03 04:26:14 -0400

    populated demos

--- static/src/demos/index.js
+++ static/src/demos/index.js
@@ -4,6 +4,7 @@ import Panel from '../carousel/panel';
 import Triangle from './triangle';
 import crispermeImg from './images/crisperme.png';
 import alzheimersImg from './images/alzheimers-children.png';
+import tft from './images/tft.png';
 import './styles';
 
 class About extends Component {
@@ -19,10 +20,9 @@ class About extends Component {
                     <h2 className={"demos_title"}>Are you looking to have a website built?</h2>
                     <p className={"demos_desc"}>I have designed, built, and deployed numerous custom websites varrying from simplistic single page designs, to large-scale applications with automated payment processing and administrative reporting. Regardless of your needs, I can assist you.</p>
                     <Carousel className={"demos_carousel"}>
-                        <Panel title={"criperme"} img={crispermeImg} desc={"Crisperme is a Toronto-based strartup that aims to connect students with researchers in their industry to aid in editing scientific papers. This application includes automated file transfer and paympent processing capabilities."}/>
-                        <Panel title={"alzheimers-children.ca"} img={alzheimersImg} desc={"Crisperme is a Toronto-based strartup that aims to connect students with researchers in their industry to aid in editing scientific papers. This application includes automated file transfer and paympent processing capabilities."}/>
-                        <Panel title={"hello"} img={crispermeImg} desc={"test2."}/>
-                        <Panel title={"world"} img={alzheimersImg} desc={"test3."}/>
+                    <Panel title={"alzheimers-children.ca"} img={alzheimersImg} desc={"Alzheimers-children.ca is a website designed to reach children of people directly affected by Alzheimer's in hopes to further pursue medical research on the topic. It is a lightweight application built to help spread awareness and collect survey data."}/>
+                        <Panel title={"criperme"} img={crispermeImg} desc={"Crisperme was a Toronto-based strartup that aimed to connect students with researchers in their industry, allowing for high-quality scientific paper editing. This application included automated file transfer and paympent processing capabilities."}/>
+                        <Panel title={"tftbuild.co"} img={tft} desc={"tftbuild.co was an interactive online tool for the initial release version of Team Fight Tactics. It was designed to easily check item builds along with champion synergies, as this data was not initially included in the game."}/>
                     </Carousel>
                     <Triangle className={"demos_angle"}/>
                     <div className={"demos_behind"}/>
