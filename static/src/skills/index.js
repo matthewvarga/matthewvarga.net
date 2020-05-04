c3474d620f7080e1c5b7cad0210feb28081c1480
matthewvarga <matthew.l.varga@gmail.com>
2020-05-03 22:38:12 -0400

    finalized mobile layout styles

--- static/src/skills/index.js
+++ static/src/skills/index.js
@@ -10,21 +10,25 @@ class Skills extends Component {
     render() {
         return (
             <div className={"skills grid_container"}>
-                <div className={"skills_content grid_content"}>
-                    <InfoCard icon={<IconCode/>} title={"Frontend"} icon_placement={"top"} className={"skills_info_card"}>
-                        <p>JavaScript (React, Redux, Marko, Jest)</p>
-                        <p>SCSS, CSS, HTML</p>
-                        <p>Webpack, Babel</p>
-                    </InfoCard>
-                    <InfoCard icon={<IconDatabase/>} title={"Backend"} icon_placement={"top"} className={"skills_info_card"}>
-                        <p>Go (golang)</p>
-                        <p>PHP, Node (express)</p>
-                        <p>MySQL, MongoDB</p>
-                    </InfoCard>
-                    <InfoCard icon={<IconInfrastructure/>} title={"Other"} icon_placement={"top"} className={"skills_info_card"}>
-                        <p>NGINX, Docker</p>
-                        <p>Python, Java, C</p>
-                    </InfoCard>
+                <div className={"grid_content"}>
+                    <h2 className={"skills_title"}>Technologies</h2>
+                    <p>Below are some technologies that I am familiar with and have working experience with.</p>
+                    <div className={"skills_content"}>
+                        <InfoCard icon={<IconCode/>} title={"Frontend"} icon_placement={"top"} className={"skills_info_card"}>
+                            <p>JavaScript (React, Redux, Marko, Jest)</p>
+                            <p>SCSS, CSS, HTML</p>
+                            <p>Webpack, Babel</p>
+                        </InfoCard>
+                        <InfoCard icon={<IconDatabase/>} title={"Backend"} icon_placement={"top"} className={"skills_info_card"}>
+                            <p>Go (golang)</p>
+                            <p>PHP, Node (express)</p>
+                            <p>MySQL, MongoDB</p>
+                        </InfoCard>
+                        <InfoCard icon={<IconInfrastructure/>} title={"Other"} icon_placement={"top"} className={"skills_info_card"}>
+                            <p>NGINX, Docker</p>
+                            <p>Python, Java, C</p>
+                        </InfoCard>
+                    </div>
                 </div>
             </div>
         );
